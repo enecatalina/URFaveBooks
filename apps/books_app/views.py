2da199d37c53db55daea655ec2f2ee78c3239f1b
Catalina Ene <ene.catalina@ymail.com>
2018-03-23 08:30:07 -0500

    update

--- apps/books_app/views.py
+++ apps/books_app/views.py
@@ -6,22 +6,24 @@ from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
 
 # Create your views here.
 def newbook(request):
-    if request.method == 'POST':
-        errors = Book.objects.book_validator(request.POST)
-        if len(errors):
-            for key, error in errors.iteritems():
-                print error
-            return redirect('books_app/newbook.html')
-        else:
-            context = {
-                'all_authors' : Author.objects.all()
-            }
-            return render(request,'logreg_app/success.html',context)
-        # the_book = Book.objects.get(id=id_book)
-        # title = request.POST['title'],
-        # author = the_book.author.id,
-        # rating = request.POST['rating']
-        # review = request.POST['review']
-        # new_author = ''
-        # Review.objects.create(tite=title,rating=rating,review=review)   
-        # return render(request, "newbook.html") 
+    
+    return render(request, "newbook.html")
+    # if request.method == 'POST':
+    #     errors = Book.objects.book_validator(request.POST)
+    #     if len(errors):
+    #         for key, error in errors.iteritems():
+    #             print error
+    #         return redirect('books_app/newbook.html')
+    #     else:
+    #         context = {
+    #             'all_authors' : Author.objects.all()
+    #         }
+    #         return render(request,'logreg_app/success.html',context)
+    #     the_book = Book.objects.get(id=id_book)
+    #     title = request.POST['title'],
+    #     author = the_book.author.id,
+    #     rating = request.POST['rating']
+    #     review = request.POST['review']
+    #     new_author = ''
+    #     Review.objects.create(tite=title,rating=rating,review=review)   
+    #     return render(request, "newbook.html") 
